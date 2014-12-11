3256a3d11f7aba4b50107120f7eff5b6098deefa
Jaqobs <me@quan-sushi.com>
2014-12-11 15:29:03 +0100

    -functional scrolling script

--- files/scrolling.js
+++ files/scrolling.js
@@ -15,15 +15,59 @@ $(document).ready( function buttonAnimation() {
 $(document).ready( function scrollingLeft() {
 	$('#controller_left').click( function() {
 		var $body = $('html, body');
+		var lastDiv = $(getLastDiv());
+		var position = lastDiv.position().left;
 		$body.stop().animate({
-			scrollLeft: (0)}, 2500);
+			scrollLeft: position}, 2500);
 	});
 });
 
 $(document).ready( function scrollingRight() {
 	$('#controller_right').click( function() {
 		var $body = $('html, body');
+		var nextDiv = $(getNextDiv());
+		var position = nextDiv.position().left;
 		$body.stop().animate({
-			scrollLeft: ('href="#slide2"')}, 2500);
-		});
+			scrollLeft: position}, 2500);
+	});
 });
+
+function getBodyPosition() {
+	var target = $('body');
+	var position = target.scrollLeft();
+	return position;
+};
+
+function getLastDiv() {
+	var body = getBodyPosition();
+	var target = "";
+	if (body <= 2000) {
+		target = "#slide1";
+	}else if (body <= 4000) {
+		target = "#slide2";
+	}else if (body <= 6000) {
+		target = "#slide3";
+	}else if (body <= 8000) {
+		target = "#slide4";
+	}else if (body <= 10000) {
+		target = "#slide5";
+	}else if (body <= 12000) {
+		target = "#slide5";
+	}
+	return target;
+};
+
+function getNextDiv() {
+	var body = getBodyPosition();
+	var target = "";
+	if (body < 2000) {
+		target = "#slide2";
+	}else if (body < 4000) {
+		target = "#slide3";
+	}else if (body < 6000) {
+		target = "#slide4";
+	}else if (body < 8000) {
+		target = "#slide5";
+	}
+	return target;
+};
